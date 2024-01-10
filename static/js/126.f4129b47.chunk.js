f832ae96b4347f7b2c8d2fb04348f9c11e1412c0
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-01-10 22:59:22 +0200

    Updates

---
+++ static/js/126.f4129b47.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_lingo=self.webpackChunklearn_lingo||[]).push([[126],{9126:(a,l,t)=>{t.d(l,{KMP:()=>e});var r=t(9983);function e(a){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"}}]})(a)}}}]);
+//# sourceMappingURL=126.f4129b47.chunk.js.map
