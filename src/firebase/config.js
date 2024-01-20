71479c997d94234dbfb7afff3784775301a920d6
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-01-20 21:47:49 +0100

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,17 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getDatabase();
-
-// ========================================
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDKym-r0bHj8a8o-8Hhk9dJhsGdeOfHcF8",
-//   authDomain: "learnlingo-9aacd.firebaseapp.com",
-//   databaseURL:
-//     "https://learnlingo-9aacd-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "learnlingo-9aacd",
-//   storageBucket: "learnlingo-9aacd.appspot.com",
-//   messagingSenderId: "456696266369",
-//   appId: "1:456696266369:web:45fc4940f3a31ad4ed5780",
-//   measurementId: "G-5HBJBPZFS3",
-// };
