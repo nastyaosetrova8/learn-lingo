893af691cd637cac394367beb39d95fd38ae3ff7
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-01-20 21:50:40 +0100

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,8 +6,7 @@ import {
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL:
-    "https://learnlingo-9aacd-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
